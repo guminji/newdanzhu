bc5589a713cfd1436e61e22a3ab420a01499bd6e
guminji <guminji@guminjideMBP.lan>
2017-06-18 23:14:12 +0800

    update

--- product/js/rank.js
+++ product/js/rank.js
@@ -27,7 +27,14 @@ class rankPop extends popBG{
             top:23
         })
         container.addChild(closeBtn);
-        function Item()
+        var allBlank_Container = this.allBlank_Container = new laya.ui.Box();
+        this.setSprite(allBlank_Container,{
+            top:495,
+            width:700,
+            height:430,
+        })
+        this.create_topRank();
+        /*function Item()
         {
             Item.__super.call(this);
             this.size(373, 60);
@@ -59,7 +66,7 @@ class rankPop extends popBG{
             cell.left = index*70
         }
         var data = [1,2,3]
-        /*for(let i = 0;i<10;i++){
+        /!*for(let i = 0;i<10;i++){
             let win_num = this['win_num'+i] = new laya.ui.Label();
             this.setSprite(win_num,{
                 fontSize:50,
@@ -70,10 +77,70 @@ class rankPop extends popBG{
                 text:i
             })
             allBlank_Container.addChild(win_num)
-        }*/
+        }*!/
         allBlank_Container.array = data;
-        container.addChild(allBlank_Container);
+        container.addChild(allBlank_Container);*/
 
+    }
+    create_topRank(){
+        var self = this;
+        function topRank_renderBox() {
+            topRank_renderBox.__super.call(this);
+            this.size(430, 70);
+            var BG = this.BG = new laya.ui.Image();
+            this.addChild(BG);
+            var names = this.names = new laya.ui.Label();
+            self.setSprite(names,{
+                width:170,
+                height:50,
+                color:'white',
+                fontSize:40
+            })
+            BG.addChild(names)
+        }
+        Laya.class(topRank_renderBox, "topRank_renderBox", Laya.Box);
+        var topContainer = this.topContainer = new laya.ui.List();
+        this.setSprite(topContainer,{
+            itemRender:topRank_renderBox,
+            repeatX : 1,
+            repeatY :3,
+            left:267,
+            top:160
+        })
+        topContainer.renderHandler = new Handler(this, updateItem);
+        function updateItem(cell,index){
+            var picName,topValue;
+            switch(index)
+            {
+                case 0:
+                    picName = 'first.png';
+                    topValue =0;
+                    break;
+                case 1:
+                    picName = 'second.png';
+                    topValue = 4;
+                    break;
+                case 2:
+                    picName = 'third.png';
+                    topValue = 3;
+                    break;
+                default:
+
+            }
+            self.setSprite(cell.BG,{
+                source:webgm.rankPop.getTexture(picName),
+                height:webgm.rankPop.getTexture(picName).height,
+                width:webgm.rankPop.getTexture(picName).width,
+
+            })
+            self.setSprite(cell.names,{
+                text:cell.dataSource.data.names?cell.dataSource.data.names:'虚位以待',
+            })
+
+        };
+        topContainer.array = [{data:{names:'顾敏吉'}},{data:{names:'顾敏吉1'}},{data:{names:'顾敏吉2'}}];
+        self.container.addChild(topContainer);
+        console.log(topContainer);
     }
     bindEvent(){
         this.closeBtn.on('click',this,function(){
