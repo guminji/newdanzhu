ecede529a728793845a99d469b69b80359cbc4db
guminji <guminji@guminjideMBP.lan>
2017-06-20 23:20:30 +0800

    update

--- product/app.js
+++ product/app.js
@@ -1,7 +1,29 @@
 var express = require('express');
 var app = express();
+var fs = require('fs')
+var path = require('path');
+var bodyParser = require('body-parser');
+var multer = require('multer');
 
+
+
+var router = express.Router();
+router.post('/sendRecord', function(req, res) {
+  console.log(req.body);
+  fs.writeFile(path.join(__dirname, 'account.js'), JSON.stringify(req.body), function (err) {
+    if (err) throw err;
+    console.log("Export Account Success!");
+    res.end()
+  });
+  //res.end()
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
+app.use(router);
 app.use(express.static(__dirname + '/'));
+//app.use(router);
+//app.use(express.static(__dirname + '/'));
 
 var server = app.listen(8080, function () {
   var host = server.address().address;
